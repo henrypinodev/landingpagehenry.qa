3962020259914351ffa25553d24c03160bfa17eb
Henry <henrypino93@gmail.com>
2026-02-15 23:53:54 -0300

    Proyecto Landing

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,6 @@
+export const evironment ={
+production : false,
+name: "qa",
+apiUrl: "https",
+enableDebug: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    name: "local",
+    apiUrl: "http://localhost:3000",
+    enableDebug: true
+};
